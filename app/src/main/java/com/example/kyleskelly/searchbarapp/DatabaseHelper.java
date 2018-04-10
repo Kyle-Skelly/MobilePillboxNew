2550a6c24c30becc01d2b6863fdd71cce2bb52d2
Kyle Skelly <kyle.skelly32@gmail.com>
2018-04-09 23:20:44 -0500

    Completed Database Helper
    Classes
    -Finished DatabaseHelper Class

--- app/src/main/java/com/example/kyleskelly/searchbarapp/DatabaseHelper.java
+++ app/src/main/java/com/example/kyleskelly/searchbarapp/DatabaseHelper.java
@@ -1,12 +1,65 @@
 package com.example.kyleskelly.searchbarapp;
 
+/**
+ * Credit of code goes to Mitch Tabian
+ * https://github.com/mitchtabian/
+ * Followed a tutorial on SQLite because of my unfamiliarity
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "mbp.db"; //Mobile Pillbox Database
     public static final String TABLE_NAME = "medications_data";
     public static final String COL1 = "ID";
-    public static final String COL2 = "DESCRIPTION";
+    public static final String COL2 = "ITEM1";
 
-    
+    public DatabaseHelper(Context context)
+    {
+        super(context,DATABASE_NAME,null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                " ITEM1 TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP IF TABLE EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    /**
+     * Method to add data into the SQLite Database, with a checker to be later implemented that
+     * will make sure that there is text entered into the Add Medications input box
+     * @param item1
+     * @return
+     */
+    public boolean addData(String item1) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2, item1);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public Cursor getListContents() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor data = db.rawQuery("SELECT * FROM " + TABLE_NAME,null);
+        return data;
+    }
 }
