0a3d22cf19fe12e43fa75663b5e04ab75775cc31
Kyle Skelly <kyle.skelly32@gmail.com>
2018-04-09 22:05:01 -0500

    Datatypes for SQLite Database
    Classes
    - Added datatypes for the SQLite
    ---COL1 and DESCRIPTION may be changed at a later date, unfamiliar with the SQLite layout so will change accordingly
    
    Database
    -Started the SQLDatabase branch

--- app/src/main/java/com/example/kyleskelly/searchbarapp/DatabaseHelper.java
+++ app/src/main/java/com/example/kyleskelly/searchbarapp/DatabaseHelper.java
@@ -3,5 +3,10 @@ package com.example.kyleskelly.searchbarapp;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "mbp.db"; //Mobile Pillbox Database
+    public static final String TABLE_NAME = "medications_data";
+    public static final String COL1 = "ID";
+    public static final String COL2 = "DESCRIPTION";
+
     
 }
