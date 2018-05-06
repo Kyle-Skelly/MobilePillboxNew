e0638896f333af2af8f4ce883ddd9c1fff152287
fardaj100 <37493323+fardaj100@users.noreply.github.com>
2018-05-06 15:24:12 -0400

    Update JSONTester.java

--- app/src/main/java/com/example/kyleskelly/searchbarapp/JSONTester.java
+++ app/src/main/java/com/example/kyleskelly/searchbarapp/JSONTester.java
@@ -12,24 +12,49 @@ import com.google.gson.*;
  * Quick class written to make sure that the json is returning properly, using gson library.
  */
 
-public class JSONTester {
 
 
-    public static void main(String[] args) throws Exception {
 
-        Gson g = new Gson();
+	public class JSONTester {
 
-        URL FDAServer = new URL("https://api.fda.gov/drug/label.json?");
+	
+		public static void main(String[] args) throws Exception {
 
-        HttpURLConnection conn = (HttpURLConnection)
-                FDAServer.openConnection();
-        conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+	        Gson g = new Gson();
+            String drug;
+	 
+	        URL FDAServer = new URL("https://api.fda.gov/drug/label.json?search=brand_name:"+ drug);
 
-        InputStreamReader inputStream = new InputStreamReader(conn.getInputStream(), "UTF-8");
+	        HttpURLConnection conn = (HttpURLConnection)
 
-        Medication advil = g.fromJson(inputStream, Medication.class);
+	                FDAServer.openConnection();
+          
+  
+
+	        conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+          	int responseCode = conn.getResponseCode();
+	//		System.out.println("Sending 'GET' request to URL : " + URL);
+			System.out.println("Response Code : " + responseCode);
 
 
-        System.out.println(advil);
-    }
-}
+	        BufferedReader inputStream = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+			
+          String inputLine;
+          String finalString = "";
+          while ((inputLine = inputStream.readLine()) != null) {
+			 System.out.println(inputLine);
+            finalString+=inputLine;
+		}
+		inputStream.close();
+     
+
+          
+          
+          
+	  Medication medicine = g.fromJson(finalString, Medication.class);
+	  System.out.println(medicine.toString());
+
+	    }
+
+	}
